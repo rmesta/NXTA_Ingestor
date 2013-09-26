d1d8faadb65a462da0a106c31aa6b59d6cb5a2f0
Andrew Galloway <andrew.galloway@nexenta.com>
2013-09-26 18:25:46 -0500

    added color functions to functions.sh for bash scripts

--- Ingestor/ingestion-scripts/functions.sh
+++ Ingestor/ingestion-scripts/functions.sh
@@ -5,4 +5,16 @@
 
 # this file contains generic functions for use in other ingestion scripts
 
-
+# colors, if you should want to use. To use, simply call the variables you want, and be SURE to call $C_RESET when
+# you reach the end of what you want to colorize. Here's an example line that would work:
+# echo -e "Testing color. ${C_UND}${C_BOLD}${C_BLUE}Blue${C_RESET}. ${C_RED}Red${C_RESET}."
+C_UND=$(tput sgr 0 1)
+C_RESET=$(tput sgr0)
+C_BOLD=$(tput bold)
+C_RED=$(tput setaf 1)
+C_GREEN=$(tput setaf 2)
+C_YELLOW=$(tput setaf 3)
+C_BLUE=$(tput setaf 4)
+C_MAGENTA=$(tput setaf 5)
+C_CYAN=$(tput setaf 6)
+C_WHITE=$(tput setaf 7)
