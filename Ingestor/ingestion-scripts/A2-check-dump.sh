4b9f2dfb9e3e1f57725714e13f5ffafd466fc000
Andrew Galloway <andrew.galloway@nexenta.com>
2013-09-30 23:09:45 -0500

    added

---
+++ Ingestor/ingestion-scripts/A2-check-dump.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Author: kirill.davydychev@nexenta.com
+# Created On: 2013-09-30
+# Last Updated On: 2013-09-30
+# Description:
+#   dump check
+
+# include generic functions file
+source /root/Collector/Ingestor/ingestion-scripts/functions.sh
+
+# name of this script - could be filename, or something unique people will recognize
+SCRIPT_NAME="A2-check-dump.sh"
+
+# put your actual code within this function, be sure to exit 0 if successful and
+# exit 1 if not
+main () {
+    BUNDLE_DIR=$1 # use BUNDLE_DIR inside here, don't use $1, just for sanity
+    WARN_FILE=${BUNDLE_DIR}/ingestor/warnings/check-dump
+
+    if [ -f "${BUNDLE_DIR}/os/dumpadm.conf" ]; then
+        DUMP_DEVICE=`expr length $(grep DUMPADM_DEVICE ${BUNDLE_DIR}/os/dumpadm.conf)`
+
+        if [ "${DUMP_DEVICE}" -lt 16 ]; then  # Empty config line will be 15 characters
+            echo "No dump device detected on the system." > $WARN_FILE
+        fi
+    fi
+}
+
+# this runs first, and does sanity checking before invoking main() function
+
+# check for necessary directory argument
+if [ -z "$1" ]; then
+    echo "${SCRIPT_NAME} failed, no directory specified."
+    exit 1
+else
+    if [ -d "$1" ]; then
+        # begin execution
+        main $1
+    else
+        # not a valid directory
+        echo "${SCRIPT_NAME} failed, invalid directory $1 specified."
+        exit 1
+    fi
+fi
