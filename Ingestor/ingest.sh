4277f8de54322f48be4ba1079ddf1ff519ad7255
Andrew Galloway <andrew.galloway@nexenta.com>
2013-10-25 11:39:57 -0500

    simple script to be put in standard PATH to ingest a tarball

---
+++ Ingestor/ingest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Andrew Galloway
+
+if [ -z "$1" ]; then
+    echo "Must specify valid filename for ingestion."
+    exit 1
+fi
+
+if [ -d "$1" ]; then
+    echo "Failed - must specify a tarball, not a directory."
+    exit 1
+fi
+
+if [ -f "$1" ]; then
+    echo "Running initial ingestion."
+    /root/Collector/Ingestor/initial_ingestion.sh $1
+
+    echo -n "You can now either run ingestor manually against the untarred directory structure, or wait - ingestor should pick up and"
+    echo " ingest the new bundle directory shortly (runs on 1m cron job but could be backed up a bit)."
+
+    # should figure out the dir and run /root/Collector/Ingestor/ingestor.sh $1, but lazy
+fi
