eac6901fa284d1a77cf8e4012019c562b9fb8ad5
Andrew Galloway <andrew.galloway@nexenta.com>
2013-11-04 19:05:15 -0600

    added logic to archive but not ingest EVAL logs

--- Ingestor/initial-ingestion.sh
+++ Ingestor/initial-ingestion.sh
@@ -69,6 +69,13 @@ mkdir -p ${SYMLINK_DIR} >/dev/null 2>&1
 
 ingest() {
     FP_TAR_FILE=$1
+    CE_PREFIX=""
+
+    if [[ "${FP_TAR_FILE}" == *EVAL* ]]; then
+        # we have a CE tarball, stick it in Community edition dir
+        CE_PREFIX="community/"
+    fi
+
     TAR_FILE=`basename ${FP_TAR_FILE}`
     UNTAR_DIR=`tar -tvf ${FP_TAR_FILE} | tail -n1 | awk '{print $6}' | awk -F'/' '{print $2}'`
     # we use the date from the bundle to prevent confusion - it is possible that due to timezone
@@ -79,42 +86,48 @@ ingest() {
     TAR_DATE=`echo ${UNTAR_DIR} | awk -F'_' '{printf $NF}' | sed 's/[^0-9.-]//g' | awk -F'.' '{printf $1}'`
 
     # first, make sure target date directories exists (probably does)
-    mkdir ${ARCHIVE_DIR}/${TAR_DATE} >/dev/null 2>&1
-    chown ${FOWNER}:${FGROUP} ${ARCHIVE_DIR}/${TAR_DATE} >/dev/null 2>&1
-    mkdir ${WORKING_DIR}/${TAR_DATE} >/dev/null 2>&1
-    chown ${FOWNER}:${FGROUP} ${ARCHIVE_DIR}/${TAR_DATE} >/dev/null 2>&1
+    mkdir ${ARCHIVE_DIR}/${CE_PREFIX}${TAR_DATE} >/dev/null 2>&1
+    chown ${FOWNER}:${FGROUP} ${ARCHIVE_DIR}/${CE_PREFIX}${TAR_DATE} >/dev/null 2>&1
+
+    if [[ "${FP_TAR_FILE}" != *EVAL* ]]; then
+        mkdir ${WORKING_DIR}/${TAR_DATE} >/dev/null 2>&1
+        chown ${FOWNER}:${FGROUP} ${ARCHIVE_DIR}/${TAR_DATE} >/dev/null 2>&1
+    fi
 
     # move to the archive location 
-    mv ${FP_TAR_FILE} ${ARCHIVE_DIR}/${TAR_DATE}/
+    mv ${FP_TAR_FILE} ${ARCHIVE_DIR}/${CE_PREFIX}${TAR_DATE}/
 
     if [ $? -gt 0 ]; then
         log "some sort of failure moving ${TAR_FILE} to archive"
         continue
     fi
 
-    chmod 660 ${ARCHIVE_DIR}/${TAR_DATE}/${TAR_FILE} >/dev/null 2>&1
-    # untar in the working location
-    cd ${WORKING_DIR}/${TAR_DATE}/ && tar -x --strip=1 -f ${ARCHIVE_DIR}/${TAR_DATE}/${TAR_FILE}
+    chmod 660 ${ARCHIVE_DIR}/${CE_PREFIX}${TAR_DATE}/${TAR_FILE} >/dev/null 2>&1
+    # untar in the working location, if not eval
 
-    if [ $? -gt 0 ]; then
-        # something went wrong
-        log "some sort of failure untarring ${ARCHIVE_DIR}/${TAR_DATE}/${TAR_FILE} to ${WORKING_DIR}/${TAR_DATE}/"
-        continue
-    else
-        # success!
-        # create a file that indicates this is a fresh untar
-        echo "`date`" > ${WORKING_DIR}/${TAR_DATE}/${UNTAR_DIR}/.just_ingested
-         chown -R ${FOWNER}:${FGROUP} ${WORKING_DIR}/${TAR_DATE}/${UNTAR_DIR} >/dev/null 2>&1
-         log "untarred|${WORKING_DIR}/${TAR_DATE}/${UNTAR_DIR}"
-        # symlink to a symlink dir, making it easier to find, maybe
-        ln -s ${WORKING_DIR}/${TAR_DATE}/${UNTAR_DIR} ${SYMLINK_DIR}
+    if [[ "${FP_TAR_FILE}" != *EVAL* ]]; then
+        cd ${WORKING_DIR}/${TAR_DATE}/ && tar -x --strip=1 -f ${ARCHIVE_DIR}/${TAR_DATE}/${TAR_FILE}
+
+        if [ $? -gt 0 ]; then
+            # something went wrong
+            log "some sort of failure untarring ${ARCHIVE_DIR}/${TAR_DATE}/${TAR_FILE} to ${WORKING_DIR}/${TAR_DATE}/"
+            continue
+        else
+            # success!
+            # create a file that indicates this is a fresh untar
+            echo "`date`" > ${WORKING_DIR}/${TAR_DATE}/${UNTAR_DIR}/.just_ingested
+             chown -R ${FOWNER}:${FGROUP} ${WORKING_DIR}/${TAR_DATE}/${UNTAR_DIR} >/dev/null 2>&1
+             log "untarred|${WORKING_DIR}/${TAR_DATE}/${UNTAR_DIR}"
+            # symlink to a symlink dir, making it easier to find, maybe
+            ln -s ${WORKING_DIR}/${TAR_DATE}/${UNTAR_DIR} ${SYMLINK_DIR}
+        fi
     fi
 }
 
 if [[ $CMDARGS == "md5" ]]; then
     # get list of potentially finished collector uploads in upload directory
     # the 2013_2014 is dirty
-    for MD5_FILE in `ls -1 ${UPLOAD_DIR}*.md5 | grep collector- | grep -v EVAL | grep '_2011-\|_2012-\|_2013-\|_2014-\|_2015-\|_2016-\|_2017-\|_2018-'`; do
+    for MD5_FILE in `ls -1 ${UPLOAD_DIR}*.md5 | grep collector- | grep | grep '_2011-\|_2012-\|_2013-\|_2014-\|_2015-\|_2016-\|_2017-\|_2018-'`; do
         # pre-set some variables we'll need
         FOUND_FP_TAR_FILE=`echo ${MD5_FILE} | sed -e 's/.md5$//g'`
 
